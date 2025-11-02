e5a4083546c9cb2c00b6ef3c6330b88ddb7bf201
MKtraining-fr <222110229+MKtraining-fr@users.noreply.github.com>
2025-11-02 05:06:58 -0500

    feat: Nouveau style 'encuentranos' (v3 - Corrections MIME et style)

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
+import './styles/find-us.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
