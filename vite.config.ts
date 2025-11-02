445e26d9fd3a7b88584f060037f18d9de5a2278e
MKtraining-fr <contact@mktraining.fr>
2025-11-02 11:52:56 +0100

    Feature/new find us style v2 (#98)
    * feat: Nouveau style 'encuentranos' avec corrections de build
    
    * fix: Alignement à gauche et fond blanc pour les blocs d'information 'encuentranos'
    
    * fix: Configuration Vite pour résoudre l'erreur MIME sur Cloudflare
    
    * fix: Tentative de correction de l'erreur MIME en commentant l'import CSS dans index.tsx
    
    ---------
    
    Co-authored-by: MKtraining-fr <222110229+MKtraining-fr@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        format: 'es',
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
           'chart-vendor': ['recharts'],
